596d34050c20183216cdeb515ee1eaebe19614b4
HongJimin <baker0929@naver.com>
2020-12-23 22:56:11 +0900

    일단 됨

---
+++ bg.js
@@ -0,0 +1,42 @@
+        // var beforeTrans = document.querySelector("#beforeTrans").value;
+        var submitBtn = document.querySelector("#TransSubmit");
+        submitBtn.addEventListener("click", DoTrans);
+
+        function DoTrans() {
+            // const xmhl = new XMLHttpRequest();
+            // xmhl.open('POST', "https://dapi.kakao.com/v2/translation/translate");
+            // xmhl.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            // xmhl.setRequestHeader('Authorization', "KakaoAK 32dc4f706e880a7711d7a57429820538");
+            // const data = {
+            //     query: "자 들어가자",
+            //     src_lang: "kr",
+            //     target_lang: "en"
+            // };
+            // xmhl.send(Object.keys(data).map(key => `${key}=${data[key]}`).join('&'));
+            // console.dir(xmhl);
+            // console.log(typeof (xmhl.responseText));
+            // console.log(xmhl);
+            // var result = JSON.stringify(xmhl);
+            // console.dir(result);
+            // document.write(result);
+            //document.getElementById('afterTrans').innerHTML = "result.translated_text";
+            var target = $("#beforeTrans").val();
+            console.log(target);
+            $.ajax({
+                    method: "POST",
+                    url: "https://dapi.kakao.com/v2/translation/translate",
+                    data: {
+                        query: target,
+                        src_lang: "kr",
+                        target_lang: "en"
+                    },
+                    headers: {
+                        Authorization: "KakaoAK 32dc4f706e880a7711d7a57429820538"
+                    }
+                })
+                .done(function (msg) {
+                    $("#afterTrans").val('');
+                    console.log(msg.translated_text[0]);
+                    $("#afterTrans").val(msg.translated_text[0]);
+                });
+        }
