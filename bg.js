ae32d1aa18aac5ce2ffc4049efdd1e5d51576f40
HongJiMin <42535969+3StarAnchovy@users.noreply.github.com>
2020-12-24 15:32:14 +0900

    이미지 추가

--- bg.js
+++ bg.js
@@ -3,24 +3,7 @@
         submitBtn.addEventListener("click", DoTrans);
 
         function DoTrans() {
-            // const xmhl = new XMLHttpRequest();
-            // xmhl.open('POST', "https://dapi.kakao.com/v2/translation/translate");
-            // xmhl.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            // xmhl.setRequestHeader('Authorization', "KakaoAK 32dc4f706e880a7711d7a57429820538");
-            // const data = {
-            //     query: "자 들어가자",
-            //     src_lang: "kr",
-            //     target_lang: "en"
-            // };
-            // xmhl.send(Object.keys(data).map(key => `${key}=${data[key]}`).join('&'));
-            // console.dir(xmhl);
-            // console.log(typeof (xmhl.responseText));
-            // console.log(xmhl);
-            // var result = JSON.stringify(xmhl);
-            // console.dir(result);
-            // document.write(result);
-            //document.getElementById('afterTrans').innerHTML = "result.translated_text";
-            var target = $("#beforeTrans").val();
+            var target = $("#beforeT  rans").val();
             var src_lang = $('#src_lang').val();
             var target_lang = $('#target_lang').val();
             console.log(target);
