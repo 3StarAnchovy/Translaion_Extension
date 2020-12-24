59029a934b5680b4741b00035ea27b5d13909571
HongJiMin <42535969+3StarAnchovy@users.noreply.github.com>
2020-12-24 13:29:38 +0900

    번역 언어 선택기능 업데이트

--- bg.js
+++ bg.js
@@ -21,14 +21,18 @@
             // document.write(result);
             //document.getElementById('afterTrans').innerHTML = "result.translated_text";
             var target = $("#beforeTrans").val();
+            var src_lang = $('#src_lang').val();
+            var target_lang = $('#target_lang').val();
             console.log(target);
+            console.log(src_lang);
+            console.log(target_lang);
             $.ajax({
                     method: "POST",
                     url: "https://dapi.kakao.com/v2/translation/translate",
                     data: {
                         query: target,
-                        src_lang: "kr",
-                        target_lang: "en"
+                        src_lang: src_lang,
+                        target_lang: target_lang
                     },
                     headers: {
                         Authorization: "KakaoAK 32dc4f706e880a7711d7a57429820538"
