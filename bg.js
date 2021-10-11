d79f3ae69d834618117348523e57c3d4fd658698
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-10-12 02:31:09 +0900

    fixFiles

--- bg.js
+++ bg.js
@@ -1,29 +1,34 @@
-        // var beforeTrans = document.querySelector("#beforeTrans").value;
-        var submitBtn = document.querySelector("#TransSubmit");
-        submitBtn.addEventListener("click", DoTrans);
+// var beforeTrans = document.querySelector("#beforeTrans").value;
+var submitBtn = document.querySelector("#TransSubmit");
+submitBtn.addEventListener("click", DoTrans);
 
-        function DoTrans() {
-            var target = $("#beforeTrans").val();
-            var src_lang = $('#src_lang').val();
-            var target_lang = $('#target_lang').val();
-            console.log(target);
-            console.log(src_lang);
-            console.log(target_lang);
-            $.ajax({
-                    method: "POST",
-                    url: "https://dapi.kakao.com/v2/translation/translate",
-                    data: {
-                        query: target,
-                        src_lang: src_lang,
-                        target_lang: target_lang
-                    },
-                    headers: {
-                        Authorization: "KakaoAK 32dc4f706e880a7711d7a57429820538"
-                    }
-                })
-                .done(function (msg) {
-                    $("#afterTrans").val('');
-                    console.log(msg.translated_text[0]);
-                    $("#afterTrans").val(msg.translated_text[0]);
-                });
+var doTrans = Document.querySelector("#beforeTrans");
+doTrans.addEventListener("keydown", DoTrans);
+
+function DoTrans() {
+    var target = $("#beforeTrans").val();
+    var src_lang = $('#src_lang').val();
+    var target_lang = $('#target_lang').val();
+
+    console.log(target);
+    console.log(src_lang);
+    console.log(target_lang);
+
+    $.ajax({ //api 호출
+        method: "POST",
+        url: "https://dapi.kakao.com/v2/translation/translate",
+        data: {
+            query: target,
+            src_lang: src_lang,
+            target_lang: target_lang
+        },
+        headers: {
+            Authorization: "KakaoAK 32dc4f706e880a7711d7a57429820538"
         }
+    })
+        .done(function (msg) {
+            $("#afterTrans").val('');
+            console.log(msg.translated_text[0]);
+            $("#afterTrans").val(msg.translated_text[0]);
+        });
+}
